257c5a078dbe4f98d6bfcc5fe63e42cc57270bf9
jngve <jngve@DESKTOP-F43VF2A>
2021-10-09 18:10:53 +0100

    commit;

---
+++ Consts/EnumActive.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace RestfullAPI.Consts
+{
+    public enum EnumActive
+    { 
+        [EnumMember(Value = "0")]
+        N = 0,
+        [EnumMember(Value = "1")]
+        Y = 1
+    }
+}
---
+++ Context/Mappers/TeamMap.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestfullAPI.Consts;
+using RestfullAPI.Models;
+using System;
+
+namespace RestfullAPI.Context.Mappers
+{
+    public class TeamMap : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> builder)
+        {
+            builder.ToTable("Team");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Id)
+                .HasColumnName("tm_id");
+
+            builder.Property(t => t.Name)
+                .HasColumnName("tm_name");
+
+            builder.Property(t => t.Active)
+                .HasColumnName("tm_active")
+                .HasConversion(t => t.ToString(), t => (EnumActive)Enum.Parse(typeof(EnumActive), t));
+
+            builder.Property(t => t.CreateDate)
+                .HasColumnName("tm_createdate");
+
+            builder.Property(t => t.ChangeDate)
+             .HasColumnName("tm_changedate");
+
+            //builder
+            //  .HasMany(p => p.ReceiveMessages)
+            //  .WithOne(um => um.Receiver)
+            //  .HasForeignKey(um => um.ReceiverId);
+
+        }
+    }
+}
