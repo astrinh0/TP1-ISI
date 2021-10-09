0a6af267803926389fcf44952a897ebbe573b5ae
Xer0-PT <joelflm@gmail.com>
2021-10-09 19:28:47 +0100

    Created Front End project

---
+++ FrontEnd/Pages/Products/ProductsPage.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Pages.Products
+{
+    public partial class ProductsPage
+    {
+        private List<string> listOfProducts = new();
+        private int quantity;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(50);
+
+            for (int i = 0; i < 10; i++)
+            {
+                listOfProducts.Add("teste");
+            }
+        }
+    }
+}
