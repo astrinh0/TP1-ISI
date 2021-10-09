4d730b43915716f363c90987537be5191ffa7138
jngve <jngve@DESKTOP-F43VF2A>
2021-10-09 15:46:26 +0100

    commit;

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Controllers
+namespace RestfullAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
