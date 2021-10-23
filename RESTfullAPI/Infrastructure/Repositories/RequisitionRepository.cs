0df4311c7c8a7b0d4a69683f2d3c80dd2d381f28
jngve <jngve@DESKTOP-F43VF2A>
2021-10-23 17:38:49 +0100

    commit;

--- RESTfullAPI/Infrastructure/Repositories/RequisitionRepository.cs
+++ RESTfullAPI/Infrastructure/Repositories/RequisitionRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace RestfullAPI.Infrastructure.Repositories
+﻿using RestfullAPI.Context;
+using RestfullAPI.Models;
+using System.Threading.Tasks;
+
+namespace RestfullAPI.Infrastructure.Repositories
 {
     public class RequisitionRepository : IRequisitionRepository
     {
+        private readonly DataContext _context;
+
+        public RequisitionRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        //public Task<bool> CreateRequisition(Requisition requisition)
+        //{
+           
+        //}
     }
 }
